1aa11796a15d2e34e4112cb3932138ce5c24e361
arisac <arisac@tomsc.com>
2020-11-12 11:22:30 -0500

    update

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -2,32 +2,74 @@ import React, { useEffect, useState } from 'react'
 const devTo = 'https://rpc1.newchain.newtonproject.org/newbridge/history?newchain_address=0x97549E368AcaFdCAE786BB93D98379f1D1561a29'
 
 const Post = ({post}) => (
+
+
   
     <article>
-      <p>from_network: {post.from_network}</p>
-      <p>from_sub_network: {post.from_sub_network}</p>
-      <p>from_chain_id: {post.from_chain_id}</p>
-      <p>to_network: {post.to_network}</p>
-      <p>to_sub_network: {post.to_sub_network}</p>
-      <p>to_chain_id: {post.to_chain_id}</p>
-      <p>from_address: {post.from_address}</p>
-      <p>from_raw_address: {post.from_raw_address}</p>
-      <p>to_address: {post.to_address}</p>
-      <p>to_raw_address: {post.to_raw_address}</p>
-      <p>from_tx_hash: {post.from_tx_hash}</p>
-      <p>from_asset_type: {post.from_asset_type}</p>
-      <p>from_asset_name: {post.from_asset_name}</p>
-      <p>from_asset_symbol: {post.from_asset_symbol}</p>
-      <p>from_asset_address: {post.from_asset_address}</p>
-      <p>to_tx_hash: {post.to_tx_hash}</p>
-      <p>to_asset_type: {post.to_asset_type}</p>
-      <p>to_asset_name: {post.to_asset_name}</p>
-      <p>to_asset_symbol: {post.to_asset_symbol}</p>
-      <p>to_asset_address: {post.to_asset_address}</p>
-      <p>amount_requested: {post.amount_requested}</p>
-      <p>amount_fulfilled: {post.amount_fulfilled}</p>
-      <p>fee: {post.fee}</p>
-      <p>status: {post.status}</p>
+      <p>
+        <span>from_network:</span> 
+        {post.from_network}({post.from_sub_network}) 
+        <span>chain_id:</span> 
+        {post.from_chain_id}
+      </p>
+      <p>
+        <span>to_network:</span> 
+        {post.to_network}({post.to_sub_network}) 
+        <span>chain_id:</span> 
+        {post.to_chain_id}
+      </p>
+      <p>
+        <span>from_address:</span> 
+        {post.from_address} 
+        <span>raw:</span> 
+        {post.from_raw_address}
+      </p>
+      <p>
+        <span>to_address:</span> 
+        {post.to_address} 
+        <span>raw:</span> 
+        {post.to_raw_address}
+      </p>
+      <p>
+        <span>from_tx_hash:</span> 
+        {post.from_tx_hash}
+      </p>
+      <p>
+        <span>from_asset:</span> 
+        {post.from_asset_type} - {post.from_asset_name}({post.from_asset_symbol})
+      </p>
+      <p>
+        <span>from_asset_address:</span> 
+        {post.from_asset_address}
+      </p>
+      <p>
+        <span>to_tx_hash:</span> 
+        {post.to_tx_hash}
+      </p>
+      <p>
+        <span>to_asset:</span> 
+        {post.to_asset_type} - {post.to_asset_name}({post.to_asset_symbol})
+      </p>
+      <p>
+        <span>to_asset_address:</span> 
+        {post.to_asset_address}
+      </p>
+      <p>
+        <span>amount_requested:</span> 
+        {post.amount_requested} {post.from_asset_symbol}
+      </p>
+      <p>
+        <span>fee:</span> 
+        {post.fee} {post.from_asset_symbol}
+      </p>
+      <p>
+        <span>amount_fulfilled:</span> 
+        {post.amount_fulfilled} {post.to_asset_symbol}
+      </p>
+      <p>
+        <span>status:</span> 
+        {post.status}
+      </p>
     </article>
 )
 
